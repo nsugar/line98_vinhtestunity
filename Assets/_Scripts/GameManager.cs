7b8d218e9b84324a18961277d8302598a6800ba6
vinhle01 <nsugarbiz@gmail.com>
2022-03-18 13:07:45 +0700

    0.1

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private List<GameObject> spArr;
+    private const int NUM_TILES_PER_COL = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spArr = new List<GameObject>();
+
+        //init gui programmatically
+        for (int i = 0; i < NUM_TILES_PER_COL; i++)
+        {
+            for (int j = 0; j < NUM_TILES_PER_COL; j++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.name = "cube" + ((j + 1) + (i * NUM_TILES_PER_COL));
+                //cube.transform.position = new Vector3(0, 0.5f, 0);
+
+                //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+                //GameObject sp = Instantiate(spritePrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                //string name = "spriteTile" + ((j + 1) + (i * NUM_TILES_PER_COL));
+                //sp.name = name;
+                //float w = sp.GetComponent<SpriteRenderer>().bounds.size.x;
+                //float h = sp.GetComponent<SpriteRenderer>().bounds.size.y;
+                //float x = 1;
+                //float y = 1;
+                ////if (j > 0) x = j + w;
+                ////if (i > 0) y = i + h;
+                //if (j > 0) x = j + w;
+                //if (i > 0) y = i + h;
+
+
+                //sp.transform.position = new Vector3(x, y, 0);
+                //spArr.Add(sp);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputHandler();
+    }
+
+    void InputHandler()
+    {
+        //Mobile
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    //logic
+                }
+            }
+        }
+
+        //PC
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    GameObject go = hit.collider.gameObject;
+                    Debug.Log(go.name);
+                }
+            }
+        }
+#endif
+    }
+}
+
---
+++ Assets/_Scripts/TODO.cs
@@ -0,0 +1,67 @@
+﻿/*
+TODO LIST
+...
+
+AR Mode
+	1. setup AR mode
+	2. test simple AR on mobile (android/ios)
+	3.test 3D model AR on real mobile device (android/ios)
+	4.find Line game unity source code
+	5. implement on dev environment
+	6. integrate LINE game with AR
+	7. test playing LINE AR game on real device (android/ios)
+...
+
+UI Setup
+	1. find assets															x
+		- 6 balls															x
+		- board																x
+		- header		
+		- dialog
+	2. tilemap																x
+	3. board: 9x9 cells(grey color)											x
+	4.header bar:
+		-score texts: best core, time, new score(white color)				
+		- black background													
+	5.setup GUI programmatically											x
+...
+
+Game Play
+	1. New game start
+	2. GameOver (win/lose)
+	3.init balls first time
+	4. Mouse detect
+	5. New position move ball
+	6. explode ball when match
+	7. add new score
+	8.random 3 balls, display them in header bar
+	9. display 3 new balls after finish moving ball (optional)
+	10.timing(optional)
+	11.initialize GUI programmatically
+...
+
+Addition effect
+	1. decline effect -> stuck moving ball
+	2. highlight effect -> selecting ball
+	3. add new score effect
+	4. new mouse position pointing effect
+	5. explosion effect to match 5 balls
+	6. pathfiding line (optional)
+	
+More Optional Feature (if has time)
+	1.Menu Home
+	2.Skin switching
+	3.Level up
+	4.map journey
+......
+
+LINE game rule
+1. use mouse to locate new position of ball
+2.explode balls if 5 balls or over are same color in a line (continously in horizontal/vertical/diagonal)
+3.earn new score for the explosion
+4.ball cannot move overlap others
+5.ball can only move 4 directions: up, down, left, right
+6.ends game when player cannot make the ball move anymore
+......
+
+ */
